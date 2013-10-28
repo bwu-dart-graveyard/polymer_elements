93a7732ef0cde203012097f85211994842023ce2
Erik Grimes <erik.grimes@gmail.com>
2013-10-27 21:42:14 -0500

    Add demo, port syntax to dart

---
+++ lib/collapse_example.dart
@@ -0,0 +1,16 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+
+library polymer.elements.collapse_example;
+
+import 'package:polymer/polymer.dart';
+
+@CustomTag('collapse-example')
+class CollapseExample extends PolymerElement {
+ 
+  CollapseExample.created() : super.created();
+ 
+  
+}
