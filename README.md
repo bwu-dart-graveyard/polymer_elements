74f4d0baddbcc2853bff3630835e48bd6ab104b9
Erik Grimes <erik.grimes@gmail.com>
2013-10-30 22:16:33 -0500

    Adjust file headers

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-polymer_elements_port
-=====================
+polymer_elements.dart 
+==========================
+
+A port of polymer.js' polymer-elements (https://github.com/Polymer/polymer-elements)
+to polymer.dart. The intent of the authors is to contribute the work
+to the dart project itself (https://www.dartlang.org).
+
--- lib/selection_example.dart
+++ lib/selection_example.dart
@@ -1,6 +1,6 @@
-// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
+// Copyright (c) 2013, the polymer_elements.dart project authors.  Please see 
+// the AUTHORS file for details. All rights reserved. Use of this source code is 
+// governed by a BSD-style license that can be found in the LICENSE file.
 
 library polymer.elements.selection_example;
 
