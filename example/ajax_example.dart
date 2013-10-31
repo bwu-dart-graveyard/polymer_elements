886ba680f6225c2bfb54b2d0e9ef057236ca5389
Erik Grimes <egrimes@healthdynamics.com>
2013-10-31 12:11:21 -0500

    Update license boilerplate

--- example/ajax_example.dart
+++ example/ajax_example.dart
@@ -1,8 +1,10 @@
 // Copyright (c) 2013, the polymer_elements.dart project authors.  Please see 
 // the AUTHORS file for details. All rights reserved. Use of this source code is 
 // governed by a BSD-style license that can be found in the LICENSE file.
+// This work is a port of the polymer-elements from the Polymer project, 
+// http://www.polymer-project.org/. 
 
-library polymer.elements.polymer_ajax_example;
+library polymer.elements.ajax_example;
 
 import 'package:polymer/polymer.dart';
 
--- lib/polymer_overlay/polymer_overlay_scale.dart
+++ lib/polymer_overlay/polymer_overlay_scale.dart
@@ -1,4 +1,10 @@
-library polymer_overlay_scale;
+// Copyright (c) 2013, the polymer_elements.dart project authors.  Please see 
+// the AUTHORS file for details. All rights reserved. Use of this source code is 
+// governed by a BSD-style license that can be found in the LICENSE file.
+// This work is a port of the polymer-elements from the Polymer project, 
+// http://www.polymer-project.org/. 
+
+library polymer.elements.polymer_overlay_scale;
 
 import 'package:polymer/polymer.dart';
 import 'package:polymer_elements/polymer_animation/polymer_animation.dart';
