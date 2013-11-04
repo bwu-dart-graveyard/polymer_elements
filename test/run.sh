42d889dc4bec83c4513904016dddc2151979303e
Erik Grimes <erik.grimes@gmail.com>
2013-11-04 08:23:39 -0600

    Add tests for polymer_ajax, polymer_selection

--- test/run.sh
+++ test/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+content_shell --dump-render-tree polymer_ajax.html
 content_shell --dump-render-tree polymer_collapse.html
 content_shell --dump-render-tree polymer_localstorage.html
+content_shell --dump-render-tree polymer_selection.html
