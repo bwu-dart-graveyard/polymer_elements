cb1a1a7bccdb9a255eadffd2820b59f75279219f
zoechi <gzoechi@gmail.com>
2013-11-09 18:20:51 +0100

    ported #51 polymer-grid-layout closes #51 started #50 polymer-layout

---
+++ example/src/polymer_grid_layout/grid_test.dart
@@ -0,0 +1,61 @@
+// Copyright (c) 2013, the polymer_elements.dart project authors.  Please see 
+// the AUTHORS file for details. All rights reserved. Use of this source code is 
+// governed by a BSD-style license that can be found in the LICENSE file.
+// This work is a port of the polymer-elements from the Polymer project, 
+// http://www.polymer-project.org/. 
+
+library polymer_elements.polymer_slide;
+
+import 'dart:html' show ContentElement, Element;
+import 'package:observe/observe.dart' show ObservableList;
+import 'package:logging/logging.dart' show Logger;
+import 'package:polymer/polymer.dart' show CustomTag, observable, PolymerElement, published;
+
+@CustomTag('grid-test')
+class GridTest extends PolymerElement {
+  GridTest.created() : super.created();
+  
+  final _logger = new Logger('grid-test');
+
+  @published List<int> layout;
+  @published ObservableList<Element> xnodes;
+  
+  
+  List<List<int>> _arrangements = [[
+                  [1, 1, 1],
+                  [2, 3, 4],
+                  [2, 3, 5]
+  ], [
+      [4, 3, 2],
+      [5, 3, 2],
+      [5, 1, 1]
+  ], [
+      [1, 1],
+      [2, 3],
+      [4, 3]
+  ]];
+  
+  @observable int outputLayout = 0;
+  
+  @override
+  void enteredView() {
+    super.enteredView();
+    this.xnodes = new ObservableList<Element>.from(this.shadowRoot.children.where(
+        (Element e) => e.localName != 'polymer-grid-layout' && e.localName != 'style')); 
+    //.from((this.$.values));
+    //this.nodes = [
+                    //  this.$.toolbar, this.$.sidebar, this.$.workspace, 
+    //  this.$.outputToolbar, this.$.output
+    //];
+    this.outputLayoutChanged(null);
+    //setInterval(this.toggleLayout.bind(this), 2500);
+  }
+  
+  void outputLayoutChanged(old) {
+    this.layout = this._arrangements[this.outputLayout];
+  }
+  
+  void toggleLayout() {
+    this.outputLayout = (this.outputLayout + 1) % this._arrangements.length;
+  }
+}
