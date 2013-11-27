b8002a74294de6ac1c0930c1694c20361b4a021e
zoechi <gzoechi@gmail.com>
2013-11-27 19:00:04 +0100

    fixes #43

--- test/polymer_collapse.dart
+++ test/polymer_collapse.dart
@@ -7,12 +7,12 @@
 
 library polymer_collapse;
 
-import "dart:html";
-import "dart:async";
-import "package:polymer/polymer.dart";
-import "package:unittest/unittest.dart";
-import "package:unittest/html_enhanced_config.dart";
-import "package:polymer_elements/polymer_collapse/polymer_collapse.dart";
+import "dart:html" show document, HtmlElement, Node, NodeTreeSanitizer;
+import "dart:async" show Future, Timer;
+import "package:polymer/polymer.dart" show initMethod, initPolymer;
+import "package:unittest/unittest.dart" show equals, expect, expectAsync0, isFalse, isNot, test;
+import "package:unittest/html_enhanced_config.dart" show useHtmlEnhancedConfiguration;
+import "package:polymer_elements/polymer_collapse/polymer_collapse.dart" show PolymerCollapse;
 
 /**
  * Sanitizer which does nothing.
@@ -36,12 +36,14 @@ void main() {
         var origH = getBoxComputedHeight();
         expect(origH, isNot(equals(0)));
         c.closed = true;
+        c.deliverChanges();
         new Future.delayed(delay, () {
           // after closed, height is 0
           expect(getBoxComputedHeight(), equals(0));
           // should be set to display: none
           expect(getBoxComputedStyle().display, equals('none'));
           c.closed = false;
+          c.deliverChanges();
           new Future.delayed(delay, () {
             // verify computed height
             expect(getBoxComputedHeight(), equals(origH));
