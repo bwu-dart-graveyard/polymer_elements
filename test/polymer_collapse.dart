4c7a0f8f5e122dd2048ec26ef10f62c736d701e5
zoechi <gzoechi@gmail.com>
2013-11-28 20:45:41 +0100

    fix test to run in polymer zone #93

--- test/polymer_collapse.dart
+++ test/polymer_collapse.dart
@@ -24,32 +24,33 @@ class NullTreeSanitizer implements NodeTreeSanitizer {
 void main() { 
   useHtmlEnhancedConfiguration();
 
-  initPolymer();
+  initPolymer().run(() {
   
-  test("polymer-collapse", () {
-    Duration delay = new Duration(milliseconds: 300);
-    var done = expectAsync0((){});
-    Timer.run(() {
-      var c = document.querySelector('#collapse') as PolymerCollapse;
-      expect(c.closed, isFalse);
-      new Future.delayed(delay,() {
-        var origH = getBoxComputedHeight();
-        expect(origH, isNot(equals(0)));
-        c.closed = true;
-        c.deliverChanges();
-        new Future.delayed(delay, () {
-          // after closed, height is 0
-          expect(getBoxComputedHeight(), equals(0));
-          // should be set to display: none
-          expect(getBoxComputedStyle().display, equals('none'));
-          c.closed = false;
-          c.deliverChanges();
+    test("polymer-collapse", () {
+      Duration delay = new Duration(milliseconds: 300);
+      var done = expectAsync0((){});
+      Timer.run(() {
+        var c = document.querySelector('#collapse') as PolymerCollapse;
+        expect(c.closed, isFalse);
+        new Future.delayed(delay,() {
+          var origH = getBoxComputedHeight();
+          expect(origH, isNot(equals(0)));
+          c.closed = true;
+          //c.deliverChanges();
           new Future.delayed(delay, () {
-            // verify computed height
-            expect(getBoxComputedHeight(), equals(origH));
-            // after opened, height is set to 'auto'
-            expect(document.querySelector('#box').style.height, equals('auto'));
-            done();
+            // after closed, height is 0
+            expect(getBoxComputedHeight(), equals(0));
+            // should be set to display: none
+            expect(getBoxComputedStyle().display, equals('none'));
+            c.closed = false;
+            //c.deliverChanges();
+            new Future.delayed(delay, () {
+              // verify computed height
+              expect(getBoxComputedHeight(), equals(origH));
+              // after opened, height is set to 'auto'
+              expect(document.querySelector('#box').style.height, equals('auto'));
+              done();
+            });
           });
         });
       });
