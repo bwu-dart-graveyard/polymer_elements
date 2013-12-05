4ca41f8a241e094875cbeda7fefde2cabf52ad7a
zoechi <gzoechi@gmail.com>
2013-12-05 16:18:30 +0100

    #66 #108

--- lib/polymer_selector/polymer_selector.dart
+++ lib/polymer_selector/polymer_selector.dart
@@ -9,8 +9,8 @@
  * The attribute 'selected' indicates which item element is being selected.
  * The attribute "multi" indicates if multiple items can be selected at once.
  * Tapping on the item element fires 'polymeractivate' event. Use the
- * 'polymerselect' event to listen for selection changes.  
- * The [CustomEvent.detail] for 'polymerselect' is a map containing 'item'
+ * 'polymer-select' event to listen for selection changes.  
+ * The [CustomEvent.detail] for 'polymer-select' is a map containing 'item'
  * and 'isSelected'.
  *
  * Example:
@@ -36,7 +36,7 @@
  *       <div class="item">Item 3</div>
  *     </polymer-selector>
  *
- * The polymer-selector element fires a 'polymerselect' event when an item's 
+ * The polymer-selector element fires a 'polymer-select' event when an item's 
  * selection state is changed. The [CustomEvent.detail] for the event is a map
  * containing 'item' and 'isSelected'.
 */
@@ -53,6 +53,34 @@ import 'package:polymer_elements/polymer_selection/polymer_selection.dart' show 
 @CustomTag('polymer-selector')
 class PolymerSelector extends PolymerElement {
   
+  /**
+   * Gets or sets the selected element.  Default to use the index
+   * of the item element.
+   *
+   * If you want a specific attribute value of the element to be
+   * used instead of index, set "valueattr" to that attribute name.
+   *
+   * Example:
+   *
+   *     <polymer-selector valueattr="label" selected="foo">
+   *       <div label="foo"></div>
+   *       <div label="bar"></div>
+   *       <div label="zot"></div>
+   *     </polymer-selector>
+   *
+   * In multi-selection this should be an array of values.
+   *
+   * Example:
+   *
+   *     <polymer-selector id="selector" valueattr="label" multi>
+   *       <div label="foo"></div>
+   *       <div label="bar"></div>
+   *       <div label="zot"></div>
+   *     </polymer-selector>
+   *
+   *     this.$.selector.selected = ['foo', 'zot'];
+   *
+   */  
   @published
   var selected = null;
   
@@ -82,8 +110,6 @@ class PolymerSelector extends PolymerElement {
   String selectedProperty = 'active';
   
   /**  Stream<CustomEvent> get onPolymerSelect =>
-      PolymerSelection._polymerSelectEvent.forTarget(this);
-   * 
    * Returns the currently selected element. In multi-selection this returns
    * an array of selected elements.
    */
@@ -96,6 +122,11 @@ class PolymerSelector extends PolymerElement {
    */
   @published
   var selectedModel = null;
+  
+  /**
+   * In single selection, this returns the selected index.
+   */
+  int selectedIndex = -1;
   
   /**
    * The target element that contains items.  If this is not set 
@@ -129,7 +160,7 @@ class PolymerSelector extends PolymerElement {
    * it is being selected.
    */
   @published
-  String activateEvent= 'click';
+  String activateEvent = 'click'; // TODO change to tap when pointerevents are supported
   
   @published
   bool notap = false;
@@ -141,11 +172,12 @@ class PolymerSelector extends PolymerElement {
   Stream<CustomEvent> get onPolymerSelect {
     PolymerSelection selection = $['selection'] as PolymerSelection;
     if(selection != null) {
-      return selection.onPolymerSelect;
+      // TODO return selection.onPolymerSelect;
     }
   }
   
-  ready() {
+  void ready() {
+    super.ready();
     this._observer = new MutationObserver(_onMutation);
     
     if (this.target == null) {
@@ -153,9 +185,7 @@ class PolymerSelector extends PolymerElement {
     }
   }
   
-//TODO revisit the polymer code - what does the where clause do? 
-  // => returns only the elements where the closure evaluates to true (where e.localName != 'template);
-  get items {
+  List<Element> get items {
     List nodes;
     if (this.target != this) {
       if (this.itemsSelector != null && this.itemsSelector.isNotEmpty) {
@@ -172,7 +202,7 @@ class PolymerSelector extends PolymerElement {
     }).toList();     
   }
   
-  targetChanged(old) {
+  void targetChanged(old) {
     if (old != null) {
       this._removeListener(old);
       this._observer.disconnect();
@@ -183,29 +213,29 @@ class PolymerSelector extends PolymerElement {
     }
   }
   
-  _addListener(node) {
+  void _addListener(node) {
     node.addEventListener(this.activateEvent, activateHandler);
   }
   
-  _removeListener(node) {
+  void _removeListener(node) {
     node.removeEventListener(this.activateEvent, activateHandler);
   }
   
-  get selection {
+  dynamic get selection {
     return this.$['selection'].selection;
   }
   
-  selectedChanged(old){
+  void selectedChanged(old){
     //(egrimes) Note: Workaround for https://code.google.com/p/dart/issues/detail?id=14496
     new Timer(Duration.ZERO, (){_updateSelected();});
     //this._updateSelected();
   }
   
-  _onMutation(records, observer){
+  void _onMutation(records, observer){
     _updateSelected();
   }
   
-  _updateSelected(){
+  void _updateSelected(){
     this._validateSelected();
     if (this.multi) {
       this.clearSelection();
@@ -219,14 +249,14 @@ class PolymerSelector extends PolymerElement {
     }
   }
   
-  _validateSelected(){
+  void _validateSelected(){
     // convert to a list for multi-selection
     if (this.multi && this.selected != null && this.selected is! List) {
       this.selected = [this.selected];
     }
   }
   
-  clearSelection() {
+  void clearSelection() {
     if (this.multi) {
       var copy = new List.from(this.selection);
       copy.forEach((s) {
@@ -239,17 +269,17 @@ class PolymerSelector extends PolymerElement {
     this.$['selection'].clear();
   }
   
-  _valueToSelection(value) {
+  void _valueToSelection(value) {
     var item = (value == null) ? 
         null : this.items[this._valueToIndex(value)];
     this.$['selection'].select(item);
   }
   
-  _updateSelectedItem() {
+  void _updateSelectedItem() {
     this.selectedItem = this.selection;
   }
   
-  selectedItemChanged(old){
+  void selectedItemChanged(old){
     if (this.selectedItem != null) {
       //TODO Figure out why this doesn't work
       //var t = this.selectedItem.templateInstance;
@@ -257,21 +287,33 @@ class PolymerSelector extends PolymerElement {
     } else {
       this.selectedModel = null;
     }
+    this.selectedIndex = (this.selectedItem != null) ?
+        this._valueToIndex(this.selected) : 1;
   }
   
-  _valueToIndex(value) {
+  int _valueToIndex(value) {
     // find an item with value == value and return it's index
-    for (var i=0, items=this.items; i< items.length; i++) {
+    for (var i = 0, items = this.items; i < items.length; i++) {
       if (this._valueForNode(items[i]) == value) {
         return i;
       }
     }
     // if no item found, the value itself is probably the index
-    return value;
+    if(value is int) {
+      return value;
+    }
+    
+    if(value is String) {
+      return int.parse(value);
+    }
+    return -1;
   }
   
-  _valueForNode(HtmlElement node) {
+  // TODO set mirrorsused
+  String _valueForNode(HtmlElement node) {
     var mirror = reflect(node);
+    // TODO (zoechi) I think we could require that a published property (attribute) is used.
+    // than we can drop reflection
     //TODO This is gross.  The alternative is to search the type heirarchy
     //for a matching variable or getter.
     try {
@@ -282,28 +324,32 @@ class PolymerSelector extends PolymerElement {
   }
   
   // events fired from <polymer-selection> object
-  selectionSelect(e, detail, node) {
+  void selectionSelect(e, detail, node) {
     this._updateSelectedItem();
     if (detail.containsKey('item')) {
       this._applySelection(detail['item'], detail['isSelected']);
     }
   }
   
-  _applySelection(HtmlElement item, isSelected) {
+  // TODO set mirrorsused
+  void _applySelection(HtmlElement item, bool isSelected) {
     if (this.selectedClass != null) {
       item.classes.toggle(this.selectedClass, isSelected);
     }
     
+    // TODO (zoechi) I think we could require that a published property (attribute) is used.
+    // than we can drop reflection
     //(egrimes) Note: It looks like Polymer.js adds the property dynamically to 
     //the item. PolymerSelector defaults selectedProperty to 'active', so users 
     //will have to explicitly set selectedProperty to an empty string to keep 
     //from blowing up. I'm not sure that's reasoable.
     if (this.selectedProperty != null && this.selectedProperty.isNotEmpty) {
-      reflect(item).setField(new Symbol('${this.selectedProperty}'), isSelected);
+//      reflect(item).setField(new Symbol('${this.selectedProperty}'), isSelected);
+      item.attributes[selectedProperty] = isSelected.toString();
     }
   }
   
-  activateHandler(e) {
+  void activateHandler(e) {
     if (!this.notap) {
       var i = this._findDistributedTarget(e.target, this.items);
       if (i >= 0) {
@@ -326,7 +372,7 @@ class PolymerSelector extends PolymerElement {
     }
   }
   
-  _addRemoveSelected(value) {
+  void _addRemoveSelected(value) {
     int i = this.selected.indexOf(value);
     if (i >= 0) {
       this.selected.removeAt(i);
