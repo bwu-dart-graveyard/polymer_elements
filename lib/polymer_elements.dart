2876911f4398d1064c4900c12721d0048a710934
Erik Grimes <erik.grimes@gmail.com>
2013-12-08 18:43:55 -0600

    Remove polymer-animation

--- lib/polymer_elements.dart
+++ lib/polymer_elements.dart
@@ -7,7 +7,6 @@ library polymer_elements;
 export 'package:polymer_elements/polymer_ajax/polymer_ajax.dart';
 export 'package:polymer_elements/polymer_ajax/polymer_xhr.dart';
 export 'package:polymer_elements/polymer_anchor_point/polymer_anchor_point.dart';
-export 'package:polymer_elements/polymer_animation/polymer_animation.dart';
 export 'package:polymer_elements/polymer_collapse/polymer_collapse.dart';
 export 'package:polymer_elements/polymer_collapse/polymer_collapse_toggle.dart';
 export 'package:polymer_elements/polymer_cookie/polymer_cookie.dart';
