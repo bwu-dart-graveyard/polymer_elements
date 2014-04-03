3efea13ea253c1c06275246ae1eb4da404759b45
zoechi <gzoechi@gmail.com>
2014-04-03 12:26:00 +0200

    #136 some minor code improvments

--- lib/polymer_signals/polymer_signals.dart
+++ lib/polymer_signals/polymer_signals.dart
@@ -1,8 +1,8 @@
-// Copyright (c) 2013, the polymer_elements.dart project authors.  Please see 
-// the AUTHORS file for details. All rights reserved. Use of this source code is 
+// Copyright (c) 2013, the polymer_elements.dart project authors.  Please see
+// the AUTHORS file for details. All rights reserved. Use of this source code is
 // governed by a BSD-style license that can be found in the LICENSE file.
-// This work is a port of the polymer-elements from the Polymer project, 
-// http://www.polymer-project.org/. 
+// This work is a port of the polymer-elements from the Polymer project,
+// http://www.polymer-project.org/.
 
 
 library polymer_elements.polymer_signals;
@@ -13,38 +13,38 @@ import 'package:polymer/polymer.dart';
 
 @CustomTag('polymer-signals')
 class PolymerSignals extends PolymerElement {
-  
+
   PolymerSignals.created() : super.created();
-  
-      enteredView() {
-        _signals.add(this);
-      }
-      
-      leftView() {
-        _signals.remove(this);
-      }
+
+  void enteredView() {
+    super.enteredView();
+    _signals.add(this);
+  }
+
+  void leftView() {
+    _signals.remove(this);
+    super.leftView();
+  }
 }
-   
+
 // private shared database
 List _signals = [];
 // signal dispatcher
-_notify(name, data) {
+void _notify(name, data) {
   // convert generic-signal event to named-signal event
-  var signal = new CustomEvent('polymer-signal' + name, 
-    canBubble: true,
-    detail: data    
-  );
+  var signal = new CustomEvent('polymer-signal' + name, canBubble: true, detail:
+      data);
   // dispatch named-signal to all 'signals' instances,
   // only interested listeners will react
   _signals.forEach((s) {
-    s.dispatchEvent(signal);  
-  });    
+    s.dispatchEvent(signal);
+  });
 }
-    
+
 
 @initMethod
-registerListener(){
-// signal listener at document
+void registerListener() {
+  // signal listener at document
   document.addEventListener('polymer-signal', (e) {
     _notify(e.detail['name'], e.detail['data']);
   });
